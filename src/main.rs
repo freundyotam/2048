f70f60d8c73c8012b45d4a3b107f1655ce76b683
yotam freund <freund.yotam@gmail.com>
2024-10-20 13:21:11 +0300

    reorginaize folders

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ mod algorithm;
 mod board;
 mod display;
 mod game;
-
+pub mod strategies {       
+    pub mod expectimax;
+}
+use crate::strategies::expectimax::ExpectimaxStrategy;
 use std::io::{stdout, BufWriter, Write};
 use std::thread;
 use std::time::Duration;
-
 use game::Game;
-mod strategies;
 
 
 
@@ -20,10 +21,11 @@ fn main() -> Result<(), std::io::Error>{
     let mut stdout = BufWriter::new(stdout_raw.lock());
     crossterm::terminal::enable_raw_mode()?;
     let board = board::Board::new();
-    let mut game: Game<5> = game::Game::new();
+    let mut game: Game<6> = game::Game::new();
 
     display::display_game(&mut stdout, &board, &game)?.flush()?;
-    let mut strategy = strategies::ExpectimaxStrategy::<5>::new();
+    let mut strategy = ExpectimaxStrategy::<6>::new();
+    let mut str = strategies::expectimax::ExpectimaxStrategy::<6>::new();
     loop {
         let (_best_score, best_move) = strategy.expectimax(&game, 3);
         game.movement(&best_move.unwrap());
